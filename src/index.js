af3caeebb406f71a2aac5af0edd6dff74dfa604b
huxinsen <xinsen.hu@gmail.com>
2019-09-17 13:40:24 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// webpack 默认支持node规范（CommonJS），同时支持 es6 规范（ESModule）
+// 打包本模块，解析出浏览器可以识别的代码
+let result = require('./a-module')
+console.log(result)
+// npx：npm 5.2后支持，可以用来调用项目内部安装的模块，如：npx webpack
